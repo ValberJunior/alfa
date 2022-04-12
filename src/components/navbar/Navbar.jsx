a8a95121005d4528fa4fd08450564aced0a8a0f0
ValberJunior <valberjr1992@gmail.com>
2022-04-12 11:55:59 -0300

    Navbar version mobile and tablet

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,53 @@
 import "./styles.scss";
+import Menu from "../../assets/menu.gif";
+import { useState } from "react";
+
 
 const Navbar = () => {
+
+  const [show, setShow] = useState(false);
+
+  const NavbarMenu = ()=>{
+    return(
+      <ul className="navbar__menu">
+          <li className="navbar__menu-item">Home</li>
+          <li className="navbar__menu-item">Sobre</li>
+          <li className="navbar__menu-item">Planos</li>
+          <li className="navbar__menu-item">Contato</li>
+      </ul>
+    )
+  }
+
+  const MenuMobile = ()=>{
+    return(
+      <ul className="navbar__menu-mobile-opts">
+      <li className="navbar__menu-mobile-items">Home</li>
+      <li className="navbar__menu-mobile-items">Sobre</li>
+      <li className="navbar__menu-mobile-items">Planos</li>
+      <li className="navbar__menu-mobile-items">Contato</li>
+      </ul>
+    )
+  }
+
   return (
-    <navbar className="navbar">
-        <ul className="navbar__menu">
-            <li className="navbar__menu-item">Home</li>
-            <li className="navbar__menu-item">Sobre</li>
-            <li className="navbar__menu-item">Planos</li>
-            <li className="navbar__menu-item">Contato</li>
-        </ul>
-    </navbar>
+    <>
+
+      <navbar className="navbar">
+
+      <div className="navbar__menu-mobile">
+        {show?MenuMobile():null}
+      </div>
+
+          {NavbarMenu()}
+
+      </navbar>
+
+      <button className="navbar__menu-button" onClick={()=>{setShow(!show)}} >
+        <img src={Menu} className="menu"/>
+      </button>
+
+
+    </>
   )
 }
 
