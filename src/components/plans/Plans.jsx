3d6301559268e6a8813ddff6b95870cace80886a
ValberJunior <valberjr1992@gmail.com>
2022-05-14 12:09:10 -0300

    create plans, wavesbottom and wavestop components

--- src/components/plans/Plans.jsx
+++ src/components/plans/Plans.jsx
@@ -1,11 +1,7 @@
 import "./styles.scss";
-
-import React from 'react'
-import Button from "../button/Button";
-import PlanCard from "../planCard/PlanCard";
 import { PLANS } from "../../Data/data";
 import Icons from "../../assets/opt.gif";
-
+import { WavesTop, PlanCard, Button, WavesBottom} from "../../components";
 
 const Plans = () => {
 
@@ -15,50 +11,59 @@ const Plans = () => {
   return (
     <section className="plans">
 
-        <h2 className="plans__title">Nossos Pacotes</h2>
+        <WavesTop/>
 
-       <article className="plans__container content">
-            
-            {/* Simple */}
-            <PlanCard>
-                <h3 className="content__titleCard">{SIMPLE.title}</h3>
-                <ul className="content__list">
-                    {SIMPLE.options.map(item=>{
-                        return(
-                            <li key={item.key}>
-                            <img src={Icons}  className="content__list-icon" alt="icon"/>
-                            {item.opt}</li>
-                        )
-                    })}
-                </ul>
-                <a href={SIMPLE.link}>
-                    <Button>
-                        Saiba mais
-                    </Button>
-                </a>
-            </PlanCard>
+        <article className="plans__container">
 
-            {/* Premium */}
-            <PlanCard>
-                <h3 className="content__titleCard" >{PREMIUM.title}</h3>
-                <ul className="content__list" >
-                    {PREMIUM.options.map(item=>{
-                        return(
-                            <li key={item.key}>
-                                <img src={Icons}  className="content__list-icon" alt="icon"/>
-                                {item.opt}
-                            </li>
-                        )
-                    })}
-                </ul>
-                <a href={PREMIUM.link}>
-                    <Button>
-                        Saiba mais
-                    </Button>
-                </a>
-            </PlanCard>
+        <h2 className="plans__container-title">Nossos Pacotes</h2>
 
-       </article>
+                <div className="plans__container-content content">
+                    
+                    {/* Simple */}
+                    <PlanCard>
+                        <h3 className="content__titleCard">{SIMPLE.title}</h3>
+                        <ul className="content__list">
+                            {SIMPLE.options.map(item=>{
+                                return(
+                                    <li key={item.key}>
+                                    <img src={Icons}  className="content__list-icon" alt="icon"/>
+                                    {item.opt}</li>
+                                )
+                            })}
+                        </ul>
+                        <a href={SIMPLE.link}>
+                            <Button>
+                                Saiba mais
+                            </Button>
+                        </a>
+                    </PlanCard>
+
+                    {/* Premium */}
+                    <PlanCard>
+                        <h3 className="content__titleCard" >{PREMIUM.title}</h3>
+                        <ul className="content__list" >
+                            {PREMIUM.options.map(item=>{
+                                return(
+                                    <li key={item.key}>
+                                        <img src={Icons}  className="content__list-icon" alt="icon"/>
+                                        {item.opt}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        <a href={PREMIUM.link}>
+                            <Button>
+                                Saiba mais
+                            </Button>
+                        </a>
+                    </PlanCard>
+
+                </div>
+
+        </article>
+      
+        <WavesBottom/>
+
     </section>
   )
 }
---
+++ src/components/waves/WavesBottom.jsx
@@ -0,0 +1,42 @@
+
+const WavesBottom = () => {
+  return (
+    <svg
+      height="100%"
+      width="100%"
+      id="svg"
+      viewBox="0 0 1440 400"
+      xmlns="http://www.w3.org/2000/svg"
+      class="transition duration-300 ease-in-out delay-150"
+    >
+      <defs>
+        <linearGradient id="gradient">
+          <stop offset="5%" stop-color="#002bdc88"></stop>
+          <stop offset="95%" stop-color="#32ded488"></stop>
+        </linearGradient>
+      </defs>
+      <path
+        d="M 0,400 C 0,400 0,133 0,133 C 205.71428571428572,171 822.8571428571429,152 1440,133 C 1440,133 1440,400 1440,400 Z"
+        stroke="none"
+        stroke-width="0"
+        fill="url(#gradient)"
+        class="transition-all duration-300 ease-in-out delay-150"
+      ></path>
+      <defs>
+        <linearGradient id="gradient">
+          <stop offset="5%" stop-color="#002bdcff"></stop>
+          <stop offset="95%" stop-color="#32ded4ff"></stop>
+        </linearGradient>
+      </defs>
+      <path
+        d="M 0,400 C 0,400 0,266 0,266 C 205.71428571428572,342 822.8571428571429,304 1440,266 C 1440,266 1440,400 1440,400 Z"
+        stroke="none"
+        stroke-width="0"
+        fill="url(#gradient)"
+        class="transition-all duration-300 ease-in-out delay-150"
+      ></path>
+    </svg>
+  );
+}
+
+export default WavesBottom
